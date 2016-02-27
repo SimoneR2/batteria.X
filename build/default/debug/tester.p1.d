74788e6e882775f0c21ba3a714b9621b737db48a
Simone <simone.righetti97@gmail.com>
2016-02-27 21:36:45 +0100

    New version1

---
+++ build/default/debug/tester.p1.d
@@ -0,0 +1,8 @@
+ build/default/debug/tester.d  \
+ build/default/debug/tester.p1:  \
+ tester.c  \
+C:/Users/simon/Desktop/LaurTec_PIC_libraries_v_3.3.1/inc/LCD_44780.h  \
+C:/Users/simon/Desktop/LaurTec_PIC_libraries_v_3.3.1/inc/delay.h  \
+pic_config.h  \
+C:/Users/simon/Desktop/LaurTec_PIC_libraries_v_3.3.1/src/delay.c  \
+C:/Users/simon/Desktop/LaurTec_PIC_libraries_v_3.3.1/src/LCD_44780.c 
--- tester.c
+++ tester.c
@@ -1,11 +1,21 @@
-/*
- * File:   tester.c
- * Author: simon
- *
- * Created on 26 febbraio 2016, 16.12
- */
+/*==============================================================================
+ *PROGRAM: Tester
+ *WRITTEN BY: Simone Righetti
+ *DATA: 27/02/2016
+ *VERSION: 1.0
+ *FILE SAVED AS: tester.c
+ *FOR PIC: 18F2550
+ *CLOCK FREQUENCY: 16 MHz
+ *PROGRAM FUNCTION: 
 
-
+======================================          
+=         INPUT AND OUTPUTS          =            
+=   RA0 => Tensione batteria (ADC)   =            
+=   RA1 => Warning LED               =            
+=   RD4-5-6-7 => ECCP (PWM Motore)   =   
+=   RB2/RB3 => CANBus                =            
+======================================
+*/
 #include <xc.h>
 #include "pic_config.h"
 #define _XTAL_FREQ 16000000
@@ -14,14 +24,44 @@
 #include "LCD_44780.c"
 #include "delay.h"
 #include "delay.c"
+#define R1 15000 //INSERIRE VALORE KOhm PARTITORE
+#define R2 7500 //INSERIRE VALORE KOhm PARTITORE
 void inizializzazione(void);
+void read_adc(void);
+void ricarica (void);
+
+unsigned char combinazioni[] = {
+    0x00,
+    0x01,
+    0x02
+};
+int lettura[] = 0;
+float current, voltage, rapporto = 0;
 
 void main(void) {
+    delay_ms(1);
+    rapporto = (R1+R2);
+    rapporto = R2/rapporto;
     inizializzazione();
+    read_adc();
     while (1) {
-        LCD_write_message("OK OK OK OK OK !");
-        delay_ms(1000);
+        read_adc();
     }
+}
+
+void read_adc(void) {
+    for (char i = 0; i < 3; i++) {
+        ADCON0 = combinazioni[i]; //disattivo conversione, imposto il canale interessato
+        ADCON0bits.ADON = 1; //attivo ADC
+        ADCON0bits.GO = 1; //inzio conversione
+        while (ADCON0bits.GODONE == 1); //attendo fine conversione
+        lettura [i] = ADRESH; //salvo il dato
+        lettura [i] = ((lettura[i] << 8) | ADRESL); //salvo il dato
+        delay_ms(5); //attesa random
+    }
+    current = (lettura[1]-lettura[0]);
+    voltage = (lettura[2]*5)/1024;
+    voltage = voltage * rapporto;
 }
 
 void inizializzazione(void) {
@@ -32,10 +72,17 @@ void inizializzazione(void) {
     TRISB = 0b00; //PORTB ALL OUTPUTS
 
     LATC = 0x00;
-    TRISC = 0b11111111;
+    TRISC = 0b11011111;
 
     LCD_initialize(16);
     LCD_write_message("inizializzazione");
     delay_ms(1000);
     LCD_clear();
+
+    ADCON0 = 0b00000000; //DISABILITO TUTTO
+    ADCON1 = 0b00001101;
+    ADCON2 = 0b10110010;
+    ADCON0bits.CHS3 = 0; //IMPOSTAZIONE DI SICUREZZA
+    ADCON0bits.CHS2 = 0; //IMPOSTAZIONE DI SICUREZZA
+    ADCON0bits.CHS1 = 0; //IMPOSTAZIONE DI SICUREZZA
 }
